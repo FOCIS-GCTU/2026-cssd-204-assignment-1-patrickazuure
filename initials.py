3643f9433ff36b46af6ab95fd48ae3647849849d
patrickazuure <patrickazuure52@gmail.com>
2026-05-31 17:05:30 +0000

    Implement function to display initials in block letters
    Added a function to print initials 'MPA' in stylized block letters.

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Prints my initials MPA in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: Patrick Azuure
+# STUDENT ID: 2425402167
+# Email: 2426402167@live.gctu.edu.gh
+# Grader:Mr.Augustus Buckman.
+#
+# On my honor, Patrick Azuure, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # This function prints the initials MPA in stylized large block letters.
+    print()
+    print("...MPA")
+    print()
+    print("...MMMM...MMMM.........PPPPPPPPPP..............AAAA.........")
+    print("...MMMM...MMMM.........PPPPPPPPPPP............AAAAAA........")
+    print("...MM.MM.MM.MM.........PP.......PP...........AA....AA.......")
+    print("...MM.MM.MM.MM.........PP.......PP...........AA....AA.......")
+    print("...MM.MM.MM.MM.........PPPPPPPPPPP..........AAAAAAAAA.......")
+    print("...MM..MMMM..MM........PPPPPPPPPP...........AAAAAAAAA.......")
+    print("...MM..MMMM..MM........PP..................AA........AA.....")
+    print("...MM...MM...MM........PP..................AA........AA.....")
+    print("...MM...MM...MM...**...PP.............**...AA........AA...**")
+    print("...MM...MM...MM...**...PP.............**...AA........AA...**")
+    print()
 
 
-
-main()
+if __name__ == "__main__":
+    main()
